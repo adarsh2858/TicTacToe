8bcc4ee74c77d9cc26b251c450d6501215f3b910
adarsh2858 <adarsh2858@gmail.com>
2021-03-09 23:10:03 +0530

    Send a notification to the user on click on invite on the multiplayer activity and have back stack trace as well and set full backup content flag to true

--- app/src/main/java/adarsh/helloworld/tictactoe/MultiplayerActivity.java
+++ app/src/main/java/adarsh/helloworld/tictactoe/MultiplayerActivity.java
@@ -1,5 +1,11 @@
 package adarsh.helloworld.tictactoe;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -7,6 +13,8 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 public class MultiplayerActivity extends AppCompatActivity {
 
@@ -16,14 +24,60 @@ public class MultiplayerActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multiplayer);
+        createNotificationChannel();
 
         inviteButton = findViewById(R.id.btn_invite);
 
         inviteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Invite Logic goes here", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Invite Logic goes here", Toast.LENGTH_SHORT).show();
+
+                // Create an explicit intent for an Activity in your app, Create an Intent for the activity you want to start
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+
+                // Create the TaskStackBuilder and add the intent, which inflates the back stack
+                TaskStackBuilder stackBuilder = TaskStackBuilder.create(getApplicationContext());
+
+                // Adds the back stack for the Intent (but not the Intent itself)
+                stackBuilder.addParentStack(MainActivity.class);
+                stackBuilder.addNextIntent(intent);
+
+                // Get the PendingIntent containing the entire back stack
+                PendingIntent pendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), "CHANNEL_ID")
+                        .setSmallIcon(R.drawable.ic_launcher_background)
+                        .setContentTitle("My notification")
+                        .setContentText("Hello World!")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                        // Set the intent that will fire when the user taps the notification
+                        .setContentIntent(pendingIntent)
+                        .setAutoCancel(true);
+
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplicationContext());
+
+                // notificationId is a unique int for each notification that you must define
+                int notificationId = 1;
+                notificationManager.notify(notificationId, builder.build());
             }
         });
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "getString(R.string.channel_name)";
+            String description = "getString(R.string.channel_description)";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("CHANNEL_ID", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
